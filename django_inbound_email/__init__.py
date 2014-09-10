9f1d4788c5f3751b978da97434b5f6c2e22105b5
Hugo Rodger-Brown <hugo@rodger-brown.com>
2014-09-10 09:58:49 +0100

    Update package description so it displays correctly on PyPI.
    The description was wrapping, so it appeared with a single '(' character
    on PyPI. I've updated it so that it's now all on a single line.

--- django_inbound_email/__init__.py
+++ django_inbound_email/__init__.py
@@ -5,8 +5,4 @@ __version__ = '0.3.3'
 __author__ = 'YunoJuno Ltd'
 __license__ = 'MIT'
 __copyright__ = 'Copyright 2014 YunoJuno'
-__description__ = (
-    "A Django app to make it easy to receive inbound emails from "
-    "a hosted transactional email service (e.g. SendGrid, Postmark, "
-    "Mandrill, etc.)."
-)
+__description__ = 'A Django app for receiving inbound emails.'
