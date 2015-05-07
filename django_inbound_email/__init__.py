ca3057ff53c6ec302dfc20ed6827ac67c979230c
Hugo Rodger-Brown <hugo@rodger-brown.com>
2015-05-07 15:16:24 +0100

    Bump version to v0.7

--- django_inbound_email/__init__.py
+++ django_inbound_email/__init__.py
@@ -1,7 +1,7 @@
 """An inbound email handler for Django."""
 
 __title__ = 'django-inbound-email'
-__version__ = '0.6.1'
+__version__ = '0.7'
 __author__ = 'YunoJuno Ltd'
 __license__ = 'MIT'
 __copyright__ = 'Copyright 2014 YunoJuno'
