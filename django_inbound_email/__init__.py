a73d6346cb55db5c002271b42bfe90403260941b
qubird <antigna9@gmail.com>
2015-05-11 18:26:56 +0100

    Bump up version to 0.8.2

--- django_inbound_email/__init__.py
+++ django_inbound_email/__init__.py
@@ -1,7 +1,7 @@
 """An inbound email handler for Django."""
 
 __title__ = 'django-inbound-email'
-__version__ = '0.8.1'
+__version__ = '0.8.2'
 __author__ = 'YunoJuno developers'
 __license__ = 'MIT'
 __copyright__ = 'Copyright 2014 YunoJuno'
