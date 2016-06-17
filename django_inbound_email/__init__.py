a04f8d0fd9fd070e44590e7d221a246a1c6da6c1
Hugo Rodger-Brown <hugo@yunojuno.com>
2016-06-17 12:49:21 +0100

    Bump version to 0.8.5

--- django_inbound_email/__init__.py
+++ django_inbound_email/__init__.py
@@ -1,7 +1,7 @@
 """An inbound email handler for Django."""
 
 __title__ = 'django-inbound-email'
-__version__ = '0.8.4'
+__version__ = '0.8.5'
 __author__ = 'YunoJuno developers'
 __license__ = 'MIT'
 __copyright__ = 'Copyright 2014 YunoJuno'
