4409f09a7ff771467747005ab3237b54f4ce041a
Gur Raunaq Singh <raunaqsoni.dev123@gmail.com>
2023-01-19 23:32:58 +0530

    feat(): updated django signals for django 4 compatibility

--- inbound_email/signals.py
+++ inbound_email/signals.py
@@ -4,10 +4,8 @@ from django.dispatch import Signal
 
 # this is fired when a new email has been successfully parsed
 # from the inbound view function.
-email_received = Signal(providing_args=['email', 'request'])
+email_received = Signal()
 
 
 # this is fired when a new email has failed validation
-email_received_unacceptable = Signal(
-    providing_args=['email', 'request', 'exception']
-)
+email_received_unacceptable = Signal()
