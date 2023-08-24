d1599ed9a3bc6f797a7afa044a683cad80fe0180
Alexei <hello@alexei.ro>
2023-08-24 15:08:14 +0100

    fix: upgrade re_path to path

--- inbound_email/urls.py
+++ inbound_email/urls.py
@@ -1,10 +1,7 @@
-try:
-    from django.urls import re_path
-except ImportError:
-    from django.conf.urls import url as re_path
+from django.urls import path
 
 from . import views
 
 urlpatterns = [
-    re_path(r'^inbound/$', views.receive_inbound_email, name='receive_inbound_email')
+    path('inbound/', views.receive_inbound_email, name='receive_inbound_email')
 ]
